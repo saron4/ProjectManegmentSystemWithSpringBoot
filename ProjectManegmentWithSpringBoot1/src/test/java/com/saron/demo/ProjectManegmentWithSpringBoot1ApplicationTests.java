eae5fac2e1ff5fa09cf3dc4dd6114d284f829771
saron4 <saronfantahun9@gmail.com>
2020-03-16 16:02:03 -0500

    initial commit

---
+++ ProjectManegmentWithSpringBoot1/src/test/java/com/saron/demo/ProjectManegmentWithSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saron.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManegmentWithSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
